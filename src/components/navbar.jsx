5110cc00883ecda6b64f67e8ba11dd114b2000a6
Drew Manley <drewmanley16@gmail.com>
2025-04-16 14:53:26 -0700

    hamburger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-<nav className="navbar">
-  <ul className="navbar-links">
-    <li><Link to="/about">WHAT IS LUNAR</Link></li>
-    <li><Link to="/educational">BUY NOW</Link></li>
-    <li><Link to="/campus">INTERACTIVE</Link></li>
-    <li><Link to="/partner">ABOUT US</Link></li>
-    <li><Link to="/contact">CONTACT</Link></li>
-  </ul>
-</nav>
+    <nav className="navbar">
+      <div className="navbar-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <li><Link to="/about">WHAT IS LUNAR</Link></li>
+        <li><Link to="/educational">BUY NOW</Link></li>
+        <li><Link to="/campus">INTERACTIVE</Link></li>
+        <li><Link to="/partner">ABOUT US</Link></li>
+        <li><Link to="/contact">CONTACT</Link></li>
+      </ul>
+    </nav>
   );
 }
 
